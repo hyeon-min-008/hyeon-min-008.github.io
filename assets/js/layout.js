a216a193b66e4685ab6f007bc1203615d08e1a6a
Roh Hyeon Min <008minimo@gmail.com>
2019-10-13 16:42:34 +0900

    upadte index.html

---
+++ assets/js/layout.js
@@ -0,0 +1,21 @@
+/************************************
+	스크롤 이벤트 박스
+************************************/
+$(window).scroll(function(){
+
+	var window_scroll_top=$(window).scrollTop();
+	var window_height=$(window).height();
+	
+	/*스크롤에 반응하는 박스*/
+	$(".page_event_box").each(function(){
+	
+		var box_offset=$(this).offset();
+		var box_top=box_offset.top;
+		
+		if(window_scroll_top+window_height-50>box_top){$(this).addClass("active");}
+		//if(window_scroll_top<=box_top&&window_scroll_top+window_height>=box_top){$(this).addClass("active");}
+		//else $(this).removeClass("active");
+		
+	});
+
+});
